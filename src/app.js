2c7c00b1828f0b56434981dae3648dd129caac85
timlohrer <me@timlohrer.de>
2023-11-11 08:28:34 +0000

    add discord url verification

--- src/app.js
+++ src/app.js
@@ -11,14 +11,19 @@ app.use(express.json());
 
 const buildPage = require('./utils/buildPage');
 
+// delete require.cache[require.resolve('./data/en/content.index.json')];
+// delete require.cache[require.resolve('./data/de/content.index.json')];
+const en = require('./data/en/content.index.json');
+const de = require('./data/de/content.index.json');
+
 app.get('/', async (req, res) => {
-    delete require.cache[require.resolve('./data/en/content.index.json')];
-    delete require.cache[require.resolve('./data/de/content.index.json')];
-    const en = require('./data/en/content.index.json');
-    const de = require('./data/de/content.index.json');
     const content = JSON.stringify({ en: en, de: de })
     res.send(buildPage('index', 'content', content));
 })
+
+app.get('/.well-known/discord', (req, res) => {
+    res.sendFile(__dirname + '/public/discord.html');
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Now online on port ${process.env.PORT}`);
