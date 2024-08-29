a5cc7beaa3ce113b0c205e5e36150542d594e968
Tim Lohrer <me@timlohrer.de>
2024-08-29 11:41:05 +0200

    remove old go framework

--- public/js/index.js
+++
@@ -1,9 +0,0 @@
-setTimeout(() => {
-    const buttons = document.getElementsByClassName('button');
-
-    for (let button of buttons) {
-        button.addEventListener('click', () => {
-            window.open(button.getAttribute('href'), '_blank');
-        });
-    }
-}, 0);
