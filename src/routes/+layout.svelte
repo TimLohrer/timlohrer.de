64fa5688603847b0817343c2672f4fb23afcb95f
Tim Lohrer <me@timlohrer.de>
2025-01-15 00:52:49 +0100

    progress: start new design implementation

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import "../app.css";
+</script>
+
+<div class="main">
+    <slot />
+    <footer class="footer">
+        <p class="copyright">Copyright {new Date().getFullYear()} - Tim Lohrer</p>
+    </footer>
+</div>
+
+
+
+
+<style>
+    .main {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        height: 100%;
+        width: 100%;
+        overflow-y: auto;
+    }
+
+    footer {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 3rem;
+    }
+
+    footer .copyright {
+        color: white;
+        font-family: 'SF-Mono-Bold';
+        font-size: 11px;
+    }
+</style>
