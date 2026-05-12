eccdf85f5fa97f2252bcc692fdfae69beffead4b
TimLohrer <me@timlohrer.de>
2026-05-12 18:14:37 +0200

    fix: footer height & update nextjs version

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,8 +2,8 @@
 
 export default function Footer() {
   return (
-    <footer className="relative bottom-[5rem] flex items-center justify-center bg-gradient-to-t from-white/50 to-white/0 dark:from-black/50 dark:to-black/0">
-      <p className="text-[.85rem] opacity-75">© {new Date().getFullYear()} Tim Lohrer. All rights reserved.</p>
-    </footer>
+    <div className="pb-10 w-full flex items-center justify-center bg-linear-to-t from-white/50 to-white/0 dark:from-black/50 dark:to-black/0">
+      <p className="text-[.85rem] opacity-75">© {new Date().getFullYear()} Tim - All rights reserved.</p>
+    </div>
   );
 }
